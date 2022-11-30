8c8dfbf672431889c7a2ecbea80146447baa9354
Gustavo Isaac Isaac <118915510+Gustavodx@users.noreply.github.com>
2022-11-30 15:24:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#EJERCICIO 1
+
+#BUCLE WHILE
+
+#INICIO DE EL NUMERO INGRESADO
+i=1
+
+#bucle while donde trabaja el iterador
+while i<=20:
+# Lectura de los numeros que cumple la condicion 
+  #Inicia en el numero 1 y termina hasta que el i tome 20
+  print(i)
+  #El avance de los numeros, en este caso de 1 en 1
+  i   +=1
+#EJERCICIO 2
+i=0
+while i<10:
+  print("Hola mundo")
+  i+=1;
